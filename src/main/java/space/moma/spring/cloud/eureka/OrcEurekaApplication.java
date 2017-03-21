d281a9d90e323ce6d18f20a1b604663bd5fb4170
ivan <ivandjp@gmail.com>
2017-03-21 17:21:37 +0800

    Initial commit

---
+++ src/main/java/space/moma/spring/cloud/eureka/OrcEurekaApplication.java
@@ -0,0 +1,12 @@
+package space.moma.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcEurekaApplication.class, args);
+	}
+}
