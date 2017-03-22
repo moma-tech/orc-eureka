7ac8dcadf76d33fdc91fd3c772fca40e3884f43a
ivan <ivandjp@gmail.com>
2017-03-22 16:42:38 +0800

    Add Eureka Server

--- src/main/java/space/moma/spring/cloud/eureka/OrcEurekaApplication.java
+++ src/main/java/space/moma/spring/cloud/eureka/OrcEurekaApplication.java
@@ -2,11 +2,13 @@ package space.moma.spring.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class OrcEurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrcEurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrcEurekaApplication.class, args);
+    }
 }
